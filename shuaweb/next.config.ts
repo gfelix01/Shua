ef7f707e2fb7b09caffbd9b369898b95f69bf42a
gfelix01 <gabriel_arturo01@hotmail.com>
2025-03-07 21:26:44 -0400

    chore: update Next.js configuration to include remote image patterns

--- shuaweb/next.config.ts
+++ shuaweb/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        pathname: "/**", // Esto permite todas las rutas de imágenes en ese dominio
+      },
+    ],
+  },
 };
 
 export default nextConfig;
