1bf4776eb7516a9fe2724f0e84686a4e9f34fa0a
gfelix01 <gabriel_arturo01@hotmail.com>
2025-03-07 22:02:39 -0400

    chore: update Next.js to version 15.2.1 and add Webpack dependencies

--- shuaweb/next.config.ts
+++ shuaweb/next.config.ts
@@ -7,8 +7,16 @@ const nextConfig = {
         hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
         pathname: "/**", // Esto permite todas las rutas de imágenes en ese dominio
       },
+
     ],
   },
+  // Elimina la configuración de Webpack si estás usando Turbopack
+  // webpack(config) {
+  //   config.optimization.minimize = false; // Desactivar minificación si está causando problemas
+  //   return config;
+  // },
+
+
 };
 
 export default nextConfig;
